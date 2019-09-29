908af3e51c23444acbf6b4d3d2bbdf7f1bec6088
David <dam.avalos@gmail.es>
2019-09-29 13:03:19 +0200

    segundoCommit

--- src/utilities/CalcularEdad.java
+++ src/utilities/CalcularEdad.java
@@ -1,5 +1,19 @@
 package utilities;
 
 public class CalcularEdad {
+	int edad;
 
+	public CalcularEdad(int edad) {
+		super();
+		this.edad = edad;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
 }
