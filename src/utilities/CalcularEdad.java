ea25bf1c3bf947abf8c8728c7bb835544c589836
David <dam.avalos@gmail.es>
2019-09-29 13:32:44 +0200

    edadMayorY_Menor

--- src/utilities/CalcularEdad.java
+++ src/utilities/CalcularEdad.java
@@ -2,6 +2,24 @@ package utilities;
 
 public class CalcularEdad {
 	int edad;
+	int edadMayor;
+	int edadMenor;
+
+	public int getEdadMayor() {
+		return edadMayor;
+	}
+
+	public void setEdadMayor(int edadMayor) {
+		this.edadMayor = edadMayor;
+	}
+
+	public int getEdadMenor() {
+		return edadMenor;
+	}
+
+	public void setEdadMenor(int edadMenor) {
+		this.edadMenor = edadMenor;
+	}
 
 	public CalcularEdad(int edad) {
 		super();
