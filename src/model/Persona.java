446e99596b85d7ec4fd1268c4d3c69bb1406517d
IVAN <ivan.job@outlook.es>
2019-09-29 13:33:31 +0200

    he modificado la clase perro un atributo y he añadido otro

--- src/model/Persona.java
+++ src/model/Persona.java
@@ -1,8 +1,8 @@
 package model;
 
 public class Persona {
-	private String ladra="";
-
+	private String raza="";
+	private String color="";
 	public Persona(String ladra) {
 		super();
 		this.ladra = ladra;
